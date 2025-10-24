f7abbe1c2f95895c1cf5af05ac0ac762f8549cc3
Lily Mara <lily.mara@discordapp.com>
2025-10-24 12:39:41 -0700

    add gc pauses to benchmark

--- crates/pyo3-gevent/benches/bench_pyo3_gevent.rs
+++ crates/pyo3-gevent/benches/bench_pyo3_gevent.rs
@@ -1,33 +1,51 @@
-use pyo3::Python;
-use std::hint::black_box;
+use pyo3::{Python, types::PyAnyMethods};
+use std::{hint::black_box, time::Duration};
 
 use criterion::{Criterion, criterion_group, criterion_main};
 
 use pyo3_gevent::thread_result::new_thread_result;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    Python::initialize();
-
-    c.bench_function("new_thread_result", |b| {
-        b.iter(|| black_box(new_thread_result::<(), ()>()))
-    });
-
-    c.bench_function("thread result send", |b| {
-        b.iter(|| {
-            let (tx, _rx) = new_thread_result::<(), ()>().unwrap();
-            _ = black_box(tx.complete_ok(black_box(())));
-        });
-    });
-
-    c.bench_function("thread result send and recv", |b| {
-        b.iter(|| {
-            let (tx, rx) = new_thread_result::<(), ()>().unwrap();
-            _ = black_box(tx.complete_ok(black_box(())));
-
-            _ = black_box(rx.wait());
-        });
+fn gevent_sleep_zero() {
+    Python::attach(|py| {
+        let gevent = py.import("gevent").unwrap();
+        gevent.getattr("sleep").unwrap().call1((0,)).unwrap();
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_with_and_without_sleep(c: &mut Criterion, name: &'static str, funk: impl Fn()) {
+    let mut group = c.benchmark_group(name);
+    group.bench_function("no-sleep", |b| b.iter(|| funk()));
+    group.bench_function("gevent.sleep(0)", |b| {
+        // GC Pause
+        gevent_sleep_zero();
+        b.iter(|| funk())
+    });
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    Python::initialize();
+
+    bench_with_and_without_sleep(c, "new_thread_result", || {
+        _ = black_box(new_thread_result::<(), ()>());
+    });
+
+    bench_with_and_without_sleep(c, "thread result send", || {
+        let (tx, _rx) = new_thread_result::<(), ()>().unwrap();
+        _ = black_box(tx.complete_ok(black_box(())));
+    });
+
+    bench_with_and_without_sleep(c, "thread result send and recv", || {
+        let (tx, rx) = new_thread_result::<(), ()>().unwrap();
+        _ = black_box(tx.complete_ok(black_box(())));
+
+        _ = black_box(rx.wait());
+    });
+}
+
+criterion_group!(
+    name = benches;
+    config = Criterion::default()
+        .measurement_time(Duration::from_secs(20));
+    targets = criterion_benchmark
+);
 criterion_main!(benches);
